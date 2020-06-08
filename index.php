f4aa169b38ac8211deb2981b3b41e725e8ea8822
Hassan Hassan <57977053+abushleakh3432-tech951@users.noreply.github.com>
2020-06-09 00:17:17 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+Welcome to the UC Irvine Machine Learning Repository!
+
+We currently maintain 497 data sets as a service to the machine learning community. You may view all data sets through our searchable interface. For a general overview of the Repository, please visit our About page. For information about citing data sets in publications, please read our citation policy. If you wish to donate a data set, please consult our donation policy. For any other questions, feel free to contact the Repository librarians.
+Supported By:
+
+	
+In Collaboration With:
+
+
+Latest News:
+09-24-2018:	Welcome to the new Repository admins Dheeru Dua and Efi Karra Taniskidou!
+04-04-2013:	Welcome to the new Repository admins Kevin Bache and Moshe Lichman!
+03-01-2010:	Note from donor regarding Netflix data
+10-16-2009:	Two new data sets have been added.
+09-14-2009:	Several data sets have been added.
+03-24-2008:	New data sets have been added!
+06-25-2007:	Two new data sets have been added: UJI Pen Characters, MAGIC Gamma Telescope
+
+Featured Data Set:  Madelon
+
+Task: Classification
+Data Type: Multivariate
+# Attributes: 500
+# Instances: 4400
+
+MADELON is an artificial dataset, which was part of the NIPS 2003 feature selection challenge. This is a two-class classification problem with continuous input variables. The difficulty is that the problem is multivariate and highly non-linear.
+Newest Data Sets:
+02-24-2020:	
+ 	Bar Crawl: Detecting Heavy Drinking
+02-18-2020:	
+ 	Bias correction of numerical prediction model temperature forecast
+12-24-2019:	
+ 	A study of Asian Religious and Biblical Texts
+12-05-2019:	
+ 	Real-time Election Results: Portugal 2019
+11-27-2019:	
+ 	QSAR fish bioconcentration factor (BCF)
+10-16-2019:	
+ 	Kitsune Network Attack Dataset
+10-11-2019:	
+ 	QSAR Bioconcentration classes dataset
+10-06-2019:	
+ 	WISDM Smartphone and Smartwatch Activity and Biometrics Dataset
+10-01-2019:	
+ 	QSAR oral toxicity
+10-01-2019:	
+ 	QSAR androgen receptor
+09-30-2019:	
+ 	Hepatitis C Virus (HCV) for Egyptian patients
+09-23-2019:	
+ 	QSAR fish toxicity
+Most Popular Data Sets (hits since 2007):
+3364520:	
+ 	Iris
+1847527:	
+ 	Adult
+1426113:	
+ 	Wine
+1266788:	
+ 	Breast Cancer Wisconsin (Diagnostic)
+1240908:	
+ 	Heart Disease
+1237224:	
+ 	Wine Quality
+1211006:	
+ 	Bank Marketing
+1192612:	
+ 	Car Evaluation
+992770:	
+ 	Human Activity Recognition Using Smartphones
+941076:	
+ 	Abalone
+888671:	
+ 	Forest Fires
+676913:	
+ 	Student Performance
