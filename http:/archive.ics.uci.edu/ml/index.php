7785f46937cbe5768e1e7384416e4893e8dd75ee
Hassan Hassan <57977053+abushleakh3432-tech951@users.noreply.github.com>
2020-06-08 23:15:44 +0300

    Create index.php

---
+++ http:/archive.ics.uci.edu/ml/index.php
@@ -0,0 +1,40 @@
+Welcome to the UC Irvine Machine Learning Repository!
+
+We currently maintain 497 data sets as a service to the machine learning community. You may view all data sets through our searchable interface. For a general overview of the Repository, please visit our About page. For information about citing data sets in publications, please read our citation policy. If you wish to donate a data set, please consult our donation policy. For any other questions, feel free to contact the Repository librarians.
+Supported By:
+
+	
+In Collaboration With:
+
+
+Latest News:
+09-24-2018:	Welcome to the new Repository admins Dheeru Dua and Efi Karra Taniskidou!
+04-04-2013:	Welcome to the new Repository admins Kevin Bache and Moshe Lichman!
+03-01-2010:	Note from donor regarding Netflix data
+10-16-2009:	Two new data sets have been added.
+09-14-2009:	Several data sets have been added.
+03-24-2008:	New data sets have been added!
+06-25-2007:	Two new data sets have been added: UJI Pen Characters, MAGIC Gamma Telescope
+
+Featured Data Set:  Australian Sign Language signs (High Quality)
+
+Task: Classification
+Data Type: Multivariate, Time-Series
+# Attributes: 22
+# Instances: 2565
+
+This data consists of sample of Auslan (Australian Sign Language) signs. 27 examples of each of 95 Auslan signs were captured from a native signer using high-quality position trackers
+Newest Data Sets:
+02-24-2020:	
+ 	Bar Crawl: Detecting Heavy Drinking
+02-18-2020:	
+ 	Bias correction of numerical prediction model temperature forecast
+12-24-2019:	
+ 	A study of Asian Religious and Biblical Texts
+12-05-2019:	
+ 	Real-time Election Results: Portugal 2019
+11-27-2019:	
+ 	QSAR fish bioconcentration factor (BCF)
+10-16-2019:	
+ 	Kitsune Network Attack Dataset
+10
